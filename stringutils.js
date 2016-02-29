1871c7c71009f3ac2678337efc2fd3b097d2618a
JannieP <jannie@pieterse.me>
2016-02-29 11:30:35 +0200

    Update stringutils.js

--- stringutils.js
+++ stringutils.js
@@ -1,15 +1,3 @@
-var StringUtils = {
-
-  CaseTypeEnum : {
-    lower: 1,
-    UPPER: 2,
-    Title: 3
-  },
-  
-  toCase : function(source,caseType){
-    if (caseType === CaseTypeEnum.Title){
-      return source.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    }
-  };
-  
+String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
